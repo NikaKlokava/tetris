7bd8652ffd20f65045bf7e4eed9f3c868abd9f84
NikaKlokava <veronika.kazakevich1997@gmail.com>
2023-07-25 15:43:08 +0300

    feat: create header and footer components

--- src/pages/game/Tetris.tsx
+++ src/pages/game/Tetris.tsx
@@ -1,12 +1,18 @@
+import { Footer } from "../../shared/components/footer";
+import { Header } from "../../shared/components/header";
 import { Gamepad, GameField, SuccessField } from "./components";
 import cl from "./tetris.module.css";
 
 export const Tetris = () => {
   return (
     <div className={cl.tetris_wrapper}>
-      <GameField />
-      <SuccessField />
-      <Gamepad />
+      <Header />
+      <div className={cl.tetris_content}>
+        <GameField />
+        <SuccessField />
+        <Gamepad />
+      </div>
+      <Footer />
     </div>
   );
 };
