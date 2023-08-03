9ca49ed31da1e409c7e690b3206d2e1a570f0cdd
NikaKlokava <veronika.kazakevich1997@gmail.com>
2023-08-03 13:02:09 +0300

    feat: update game success and add gameover

--- src/pages/game/Tetris.tsx
+++ src/pages/game/Tetris.tsx
@@ -1,30 +1,40 @@
 import { Footer } from "../../shared/components/footer";
 import { Header } from "../../shared/components/header";
 import { Gamepad, GameField, SuccessField } from "./components";
-import cl from "./tetris.module.css";
 import { useStage } from "../../shared/hooks/useStage";
 import { useInterval } from "../../shared/hooks/useInterval";
 import { useEffect, useState } from "react";
 import { useScore } from "../../shared/hooks/useScore";
+import cl from "./tetris.module.css";
 
 export const Tetris = () => {
   const [delay, setDelay] = useState<number | null>(null);
-  const [level, setLevel] = useState<number>(0);
-  const { stage, completedRow, dropFigure, startGame, moveFigure, rotate } =
-    useStage();
+  const [level, setLevel] = useState(0);
+  const {
+    stage,
+    completedRow,
+    gameOver,
+    dropFigure,
+    startGame,
+    moveFigure,
+    rotate,
+  } = useStage();
 
-  const { score, totalRows } = useScore({ completedRow, level });
+  const { score, totalRows, updateScore } = useScore({ completedRow, level });
 
   useEffect(() => {
     if (totalRows > (level + 1) * 1) {
       setLevel((prev) => prev + 1);
       setDelay(1000 / (level + 1) + 200);
     }
-  }, [totalRows, level]);
+    gameOver && setDelay(null);
+  }, [totalRows, level, gameOver]);
 
   const handleStartGameClick = () => {
     startGame();
-    setDelay(1000);
+    setLevel(0);
+    setDelay(1200);
+    updateScore();
   };
 
   const handleMouseUp = () => {
@@ -48,6 +58,7 @@ export const Tetris = () => {
           rows={totalRows}
           level={level}
           score={score}
+          gameOver={gameOver}
         />
         <Gamepad
           drop={handleDropFigureClick}
